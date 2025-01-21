634746a68578b9aff3dc0b6a13fdad5a0c97e795
Teodora <tedy.aa@abv.bg>
2025-01-21 13:16:32 +0200

    refactor

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="ap" :class="typeof this.$store.state.weather.list[0].main !='undefined'
-  && this.$store.state.weather.list[0].main.temp - 272 > 25 ? 'warm' : (this.$store.state.weather.list[0].main.temp - 272 <10? '':'r')">
+  <div id="ap" :class="wrapperClass">
     <nav-bar/>
     <main>
       <div class="search-box mt-5">
@@ -12,14 +11,14 @@
         />
         <button class="btn btn-success mt-2" @click="fetchWeather(query)" >Search</button>
       </div>
-      <div class="weather-wrap" v-if="this.$store.state.weather.list && typeof this.$store.state.weather.list[0].main !='undefined'">
+      <div class="weather-wrap" v-if="hasWeather">
         <div class="location-box">
-          <div class="location">{{ this.$store.state.weather.city.name}},{{ this.$store.state.weather.city.country }}</div>
+          <div class="location">{{ cityName}},{{ countryName }}</div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">{{ Math.round((this.$store.state.weather.list[0].main.temp - 272)) }}°C</div>
-          <div class="weather">{{ this.$store.state.weather.list[0].weather[0].description }}</div>
+          <div class="temp">{{ temperatureFormatted }}°C</div>
+          <div class="weather">{{ weather }}</div>
 
         </div>
       </div>
@@ -44,12 +43,32 @@ export default {
       beforeQuery:'sofia'
     }
   },
+  computed:{
+    temperatureFormatted(){
+      return Math.round((this.$store.state.weather.list[0].main.temp - 272))
+    },
+    weather(){
+      return this.$store.state.weather.list[0].weather[0].description
+    },
+    cityName(){
+      return this.$store.state.weather.city.name
+    },
+    countryName(){
+      return this.$store.state.weather.city.country
+    },
+    hasWeather(){
+      return this.$store.state.weather.list && typeof this.$store.state.weather.list[0].main !='undefined'
+    },
+    wrapperClass(){
+      return typeof this.$store.state.weather.list[0].main !='undefined'
+      && this.$store.state.weather.list[0].main.temp - 272 > 25 ? 'warm' : (this.$store.state.weather.list[0].main.temp - 272 <10? '':'r')
+    }
+  },
   methods: {
     ...mapState([]),
     ...mapActions([
       'fetchWeather',
     ]),
-
     dateBuilder() {
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",]
