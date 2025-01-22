144f7051699d3c4a4674dd09a5bc935eea630249
Teodora <tedy.aa@abv.bg>
2025-01-22 15:56:44 +0200

    changed components order

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,23 +1,54 @@
-
 <template>
-<div>
-  <div class="weather-info text-white">
+  <div class="weather-info text-white mt-10 mb-10">
     <div class="row ml-5 mr-5">
       <div class="col-6">
-        <p>RealFeel</p>
+       <div class="d-flex justify-content-between">
+         <p>RealFeel</p>
+         <p>46°</p>
+       </div>
+        <hr />
       </div>
       <div class="col-6">
-        <p>RealFeel Shade™</p>
+        <div class="d-flex justify-content-between">
+          <p>RealFeel</p>
+          <p>46°</p>
+        </div>
+        <hr />
+      </div>
+    </div>
+    <div class="row ml-5 mr-5">
+      <div class="col-6">
+        <div class="d-flex justify-content-between">
+          <p>RealFeel</p>
+          <p>46°</p>
+        </div>
+        <hr />
       </div>
       <div class="col-6">
-        <p>Max UV Index</p>
+        <div class="d-flex justify-content-between">
+          <p>RealFeel</p>
+          <p>46°</p>
+        </div>
+        <hr />
+      </div>
+    </div>
+    <div class="row ml-5 mr-5">
+      <div class="col-6">
+        <div class="d-flex justify-content-between">
+          <p>RealFeel</p>
+          <p>46°</p>
+        </div>
+        <hr />
       </div>
       <div class="col-6">
-        <p>Wind</p>
+        <div class="d-flex justify-content-between">
+          <p>RealFeel</p>
+          <p>46°</p>
+        </div>
+        <hr />
       </div>
     </div>
   </div>
-</div>
 </template>
 <script>
 export default {
@@ -27,15 +58,15 @@ export default {
 <style>
 .weather-info{
   display: inline-block;
-  padding: 10px 25px;
+  padding: 40px;
   color: #FFF;
   font-size: 18px;
   font-weight: 900;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.25);
   border-radius: 16px;
-  margin: 30px 0px;
   box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  height: 100%;
+  height: fit-content;
+  width: 100%;
 }
 </style>
