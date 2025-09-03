37ac88253835082bd5accf891b3c0b731f7ec936
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2025-09-03 11:10:31 +0300

    changed appearance

--- src/components/forecast/ForecastCard.vue
+++ src/components/forecast/ForecastCard.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="text-center forecast-card">
   <p class="text-white">{{dateBuilderDD(forecast.dt)}}</p>
-  <img :src="forecastIcon" width="90" height="90"/>
+  <img :src="forecastIcon" :alt="forecastIconAlt" width="90" height="90"/>
   <p><span class="min-temp">{{kelvinToCelsius(forecast.main.temp_min)}}°</span> / {{kelvinToCelsius(forecast.main.temp_max)}}°</p>
 </div>
 </template>
@@ -17,16 +17,10 @@ export default {
   },
   computed:{
     forecastIcon(){
-      switch(this.forecast.weather[0].main){
-        case 'Clouds':
-            return require('../../images/cloud.png')
-        case 'Clear':
-          return require('../../images/clear.png')
-        case 'Rain':
-          return require('../../images/rain.png')
-        default:
-          return '../assets/logo.png'
-      }
+      return `https://openweathermap.org/img/wn/${this.forecast.weather[0].icon}@2x.png`
+    },
+    forecastIconAlt(){
+      return this.forecast.weather[0].main
     }
   }
 }
